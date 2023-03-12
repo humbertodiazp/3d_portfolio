908425dc4cda4582236f467b08d3d35e58f84a32
Humberto <hdkaratechop@gmail.com>
2023-03-11 23:48:00 -0800

    update navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Adrian &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
+            Humberto &nbsp;
+            <span className='sm:block hidden'> | Ruby on Rails </span>
           </p>
         </Link>
 
